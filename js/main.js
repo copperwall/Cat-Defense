f2571e028004bb85c4e5bbec80883fe1a4fa0e51
Addion Grant <addison08@gmail.com>
2017-01-21 20:38:15 -0800

    Use new appraoch

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var game = new Phaser.Game(1000, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1200, 900, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
     // Load env assets
     game.load.image('tile', 'assets/env/tile.png');
-    game.load.iamge('room', 'assets/env/room.png');
+    game.load.image('room', 'assets/env/room.png');
     game.load.spritesheet('heart', 'assets/env/heart-sprite.png');
 
     // Load items assets
@@ -18,11 +18,60 @@ function preload() {
 }
 
 function create() {
+    // The room
+    room = game.add.sprite(0, 0, 'room');
+    room.scale.setTo(game.width/room.width, game.height/room.height);
     
+    // The thing the cats march to
+    target = game.add.sprite(0, 0, 'tile');
+    target.scale.setTo(1, game.height/target.height);
+
+    // The tiles where you can place your defense
+    tiles = game.add.group();
+    tiles.createMultiple(50, 'tile', 0, true);
+    tiles.align(10, 5, 90, 90, Phaser.CENTER);
+    tiles.x = (game.width - 900)/2;
+    tiles.y = (game.height - 450)/2;
+
+    
+    
+    cat = launchCat(game.width, 100);
+/*
+    cat = game.add.sprite(game.width, 0, 'garfield');
+    cat.animations.add('left', [0, 1], 2, true);
+    game.physics.arcade.enable(cat);
+    cat.body.velocity.x = -50;
+*/
+    game.physics.arcade.moveToObject(cat, target);
 }
 
 function update() {
-    
+    cat.animations.play('left');
 }
+
+
+function launchCat(x, y) {
+    var cat = new Cat(game);
+    cat.x = x;
+    cat.y = y;
+    return cat;
+}
+
+
+var Cat = function (game, x, y) {
+    Phaser.Sprite.call(this, game, x, y,
+        ['garfield',
+         'marie',
+         'mimi',
+         'whitey'][game.rnd.integerInRange(0,3)]);
+
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+}
+
+Cat.prototype = Object.create(Phaser.Sprite.prototype);
+Cat.prototype.constructor = Cat;
+
+
+
 
 
---
+++ js/newmain.js
@@ -0,0 +1,75 @@
+var GameState = function(game) {
+        this.MAX_CATS = 3; // number of cats
+};
+
+GameState.prototype.preload = function() {
+    this.game.load.image('tile', 'assets/env/tile.png');
+    this.game.load.image('room', 'assets/env/room.png');
+    this.game.load.spritesheet('heart', 'assets/env/heart-sprite.png');
+
+    // Load items assets
+    this.game.load.image('yarn', 'assets/items/yarn-small.jpg');
+
+    // Load cat assets
+    this.game.load.spritesheet('garfield', 'assets/cats/garfield-sprite.png', 100, 100);
+    this.game.load.spritesheet('marie', 'assets/cats/marie-sprite.png', 100, 100);
+    this.game.load.spritesheet('mimi', 'assets/cats/mimi-sprite.png', 100, 100);
+    this.game.load.spritesheet('whitey', 'assets/cats/whitey-sprite.png', 100, 100);
+};
+
+GameState.prototype.create = function() {
+    // Set stage background to something sky colored
+    this.game.stage.backgroundColor = 0x4488cc;
+
+    // Create a group to hold the missile
+    this.catGroup = this.game.add.group();
+};
+
+GameState.prototype.update = function() {
+    // If there are fewer than MAX_MISSILES, launch a new one
+    if (this.catGroup.countLiving() < this.MAX_CATS) {
+        // Set the launch point to a random location below the bottom edge
+        // of the stage
+        var newcat = this.launchCat(this.game.width - 100, 
+			this.game.rnd.integerInRange(50, this.game.height-50));
+		this.game.physics.arcade.moveToObject(newcat, target);
+    }
+};
+
+GameState.prototype.launchCat = function(x, y) {
+    // // Get the first dead missile from the missileGroup
+    var cat = this.catGroup.getFirstDead();
+
+    // If there aren't any available, create a new one
+    if (cat === null) {
+        cat = new Cat(this.game);
+        this.catGroup.add(cat);
+    }
+
+    // Revive the missile (set it's alive property to true)
+    // You can also define a onRevived event handler in your explosion objects
+    // to do stuff when they are revived.
+    cat.revive();
+
+    // Move the missile to the given coordinates
+    cat.x = x;
+    cat.y = y;
+
+    return cat;
+};
+
+var Cat = function (game, x, y) {
+    Phaser.Sprite.call(this, game, x, y,
+        ['garfield',
+         'marie',
+         'mimi',
+         'whitey'][game.rnd.integerInRange(0,3)]);
+
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+}
+
+Cat.prototype = Object.create(Phaser.Sprite.prototype);
+Cat.prototype.constructor = Cat;
+
+var game = new Phaser.Game(1200, 900, Phaser.AUTO, 'game');
+game.state.add('game', GameState, true);
