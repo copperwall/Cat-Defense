fae6410e885c2ec2edd4ebee69bba23ae09fcf4b
Addion Grant <addison08@gmail.com>
2017-01-21 17:38:29 -0800

    Reorganize and load assets

--- js/main2.js
+++ js/main2.js
@@ -1,15 +1,28 @@
 var game = new Phaser.Game(1000, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+    // Load env assets
+    game.load.image('tile', 'assets/env/tile.png');
+    game.load.iamge('room', 'assets/env/room.png');
+    game.load.spritesheet('heart', 'assets/env/heart-sprite.png');
+
+    // Load items assets
+    game.load.image('yarn', 'assets/items/yarn-small.jpg');
+
+    // Load cat assets
+    game.load.spritesheet('garfield', 'assets/cats/garfield-sprite.png', 100, 100);
+    game.load.spritesheet('marie', 'assets/cats/marie-sprite.png', 100, 100);
+    game.load.spritesheet('mimi', 'assets/cats/mimi-sprite.png', 100, 100);
+    game.load.spritesheet('whitey', 'assets/cats/whitey-sprite.png', 100, 100);
 
 }
 
 function create() {
-
+    
 }
 
 function update() {
-
+    
 }
 
 
